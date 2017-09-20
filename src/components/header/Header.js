4c4cad6f6c30fb2b93bec359e8236609a229d083
thiagodroz <thiago.droz@gmail.com>
2017-09-20 14:20:22 -0300

    Creating a toggle menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import './Header.css';
 
+import Menu from './Menu';
+
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuIsOpen: false
+    };
+
+    this.onMenuClick = this.onMenuClick.bind(this);
+    this.onCloseMenu = this.onCloseMenu.bind(this);
+  }
+
+  onMenuClick() {
+    this.setState({
+      ...this.state,
+      menuIsOpen: true
+    });
+  }
+
+  onCloseMenu() {
+    this.setState({
+      ...this.state,
+      menuIsOpen: false
+    });
+  }
+
   render() {
+    const { menuIsOpen } = this.state;
+
     return (
       <header className="header">
         <div className="header__brand">
           Holy Git!
         </div>
-        <div className="header__menu">
+        <div className="header__menu" onClick={ !menuIsOpen && this.onMenuClick }>
           <i className="material-icons">menu</i>
+          <Menu isOpen={ menuIsOpen } closeMenu={ this.onCloseMenu } />
         </div>
       </header>
     );
---
+++ src/components/header/Menu.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Menu.css';
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setMenuRef = this.setMenuRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  setMenuRef(node) {
+    this.menuRef = node;
+  }
+
+  handleClickOutside(event) {
+    const { closeMenu, isOpen } = this.props;
+
+    if (isOpen && this.menuRef && !this.menuRef.contains(event.target)) {
+      setTimeout(() => {
+        closeMenu();
+      }, 10);
+    }
+  }
+
+  render() {
+    return (
+      <nav
+        className={ `header-menu__nav ${ this.props.isOpen ? 'header-menu__nav-open' : '' }` }
+        ref={ this.setMenuRef }>
+          <ul className="header-menu__list">
+            <li className="header-menu__element">
+              <a href="/" className="header-menu__link">Home</a>
+            </li>
+            <li className="header-menu__element">
+              <a href="/about" className="header-menu__link">About</a>
+            </li>
+          </ul>
+      </nav>
+    );
+  }
+}
