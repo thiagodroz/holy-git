de0024d80f0e047b520642c920c85ae1412a112d
Thiago Nogueira <thiago.droz@gmail.com>
2017-09-28 23:51:06 -0300

    Adding GA tracking

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import Home from './home/Home';
 import Profile from './profile/Profile';
@@ -7,16 +8,22 @@ import About from './about/About';
 import NotFound from './not-found/NotFound';
 
 export default class RoutesComponent extends Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={ Home } />
-                    <Route path="/profiles/:profileName/" component={ Profile } />
-                    <Route path="/about/" component={ About } />
-                    <Route path="*" component={ NotFound } />
-                </Switch>
-            </Router>
-        );
+  pageView() {
+    if (window.location.host === "holy-git.com") {
+      ReactGA.pageview(window.location.hash);
     }
+  }
+
+  render() {
+    return (
+      <Router onUpdate={ this.pageView }>
+        <Switch>
+          <Route path="/" exact component={ Home } />
+          <Route path="/profiles/:profileName/" component={ Profile } />
+          <Route path="/about/" component={ About } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </Router>
+    );
+  }
 }
