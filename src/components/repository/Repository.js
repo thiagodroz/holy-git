63e73e48ddd62fb5d5861c793d52f57a207e3a4a
Thiago Nogueira <thiago.droz@gmail.com>
2017-09-30 04:02:02 -0300

    Show repository info

--- src/components/repository/Repository.js
+++ src/components/repository/Repository.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
+import RepositoryDetail from './RepositoryDetail';
 import language from './Languages';
 import './Repository.css';
 import './Languages.css';
 
 export default class Repository extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleRepository = this.toggleRepository.bind(this);
+
+    this.state = { repositoryIsOpened: false };
+  }
+
+  toggleRepository() {
+    this.setState({
+      repositoryIsOpened: !this.state.repositoryIsOpened
+    });
+  }
+
   render() {
     const repository = this.props.data;
 
@@ -13,9 +29,12 @@ export default class Repository extends Component {
     }
 
     return (
-      <div className="repository">
-        <i className={ `repository__language-icon ${ language(repository.language) }` }></i>
-        <h3 className="repository__name">{ repository.name }</h3>
+      <div>
+        <div className="repository" onClick={ this.toggleRepository }>
+          <i className={ `repository__language-icon ${ language(repository.language) }` }></i>
+          <h3 className="repository__name">{ repository.name }</h3>
+        </div>
+        <RepositoryDetail repository={ repository } isOpened={ this.state.repositoryIsOpened } />
       </div>
     );
   }
